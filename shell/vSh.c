18e53c77773312eb94734e718ed991580cad4102
student <student@localhost.localdomain>
2017-09-24 22:34:13 -0600

    Need to look through path

--- shell/vSh.c
+++ shell/vSh.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include "vToke.h"
 #include "vSh.h"
 
@@ -26,13 +27,29 @@ void vFree(char **cmd) {
   free(cmd);
 }
 
+char *vGetPath(char **envp) {
+  char *path = envp[26];
+  char **pathToke = vToke(path, '=');
+  char *files = pathToke[1];
+  return files;
+  
+}
+
 int main(int argc, char** argv, char** envp) {
-  char **cmd;
-  char *cmdStr;
   char buf[MAXBUF];
+
+  char **cmd;  // the tokenized command
+  char **dir;  // the tokenized directories in path
+  
+  char *cmdStr;
+  char *exitStr = "exit";
+  
+  char *path = vGetPath(envp); // the path environment variable
+
   int len;
   int i;
-  char *exitStr = "exit";
+
+  dir = vToke(path, ':');
   
   while(1) {
     // print prompt
@@ -40,10 +57,13 @@ int main(int argc, char** argv, char** envp) {
     
     // read in command
     len = read(0, buf, (size_t)MAXBUF);
+    
     // if command is empty, start from the beginning
     if(len == 1) {
       continue;
     }
+
+    //terminate command
     buf[len-1] = '\0';
     cmdStr = buf;
     
@@ -54,10 +74,27 @@ int main(int argc, char** argv, char** envp) {
     
     // tokenize the command
     cmd = vToke(cmdStr, ' ');
-    for (i = 0; cmd[i] != '\0'; i++) {
-      printf("%s\n", cmd[i]);
+
+    // 
+
+    
+    int rc = fork();
+    if (rc < 0) {
+      // fork failed, exit
+      exit(1);
+    } else if (rc == 0) {
+      // child
+      int returnVal = execve(cmd[0], &cmd[0], envp);
+      write(1, "Command not found\n", 18);
+      exit(0);
+    } else {
+      // parent
+      int wc = wait(NULL);
     }
     
-  }
+    vFree(cmd);
+    cmd = NULL;
+  }  // end of while
+  
   return 0;
-}
+}  // end of main
